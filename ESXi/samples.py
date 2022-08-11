0b70ab556304f19ccfa6ca0b3e3f8c090cc5192c
Nathan GASC <nathan.gasc@advens.fr>
2022-08-11 16:55:02 +0200

    Added Ubuntu Desktop template

--- ESXi/samples.py
+++ ESXi/samples.py
@@ -3,13 +3,15 @@ TYPES = {
     # [nom, fichier sample terraform, fichier sample ansible]
     "win": ["terraform/winSample.tf", "../Vagrant/resources/guacamole/winSample.xml"],
     "dc": ["terraform/dcSample.tf", "../Vagrant/resources/guacamole/dcSample.xml"],
-    "ubuntu": ["terraform/linuxSample.tf", "../Vagrant/resources/guacamole/ubuntuSample.xml"]
+    "ubuntu": ["terraform/linuxSample.tf", "../Vagrant/resources/guacamole/ubuntuSample.xml"],
+    "ubuntuDsk": ["terraform/ubuntuDskSample.tf", "../Vagrant/resources/guacamole/ubuntuDskSample.xml"]
 }
 
 ROLES = {
     "ubuntu": ["ubuntuServerSample.yml"],
     "dc": ["dcSample.yml"],
     "win": ["winSample.yml"],
+    "ubuntuDsk": ["ubuntuDskSample.yml"],
 
     "joinDomain": ["joinDomainSample.yml"],
     "createDomain": ["createDomainSample.yml"],
---
+++ ESXi/terraform/ubuntuDskSample.tf
@@ -0,0 +1,42 @@
+resource "esxi_guest" "<name>" {
+  guest_name = "<name>"
+  disk_store = var.esxi_datastore
+  guestos    = "ubuntu-64"
+
+  boot_disk_type = "thin"
+
+  memsize            = "4096"
+  numvcpus           = "2"
+  resource_pool_name = "/"
+  power              = "on"
+  clone_from_vm = "UbuntuDesktop2004"
+
+    provisioner "remote-exec" {
+    inline = [
+      "sudo ifconfig eth0 up && echo 'eth0 up' || echo 'unable to bring eth0 interface up",
+      "sudo ifconfig eth1 up && echo 'eth1 up' || echo 'unable to bring eth1 interface up"
+    ]
+
+    connection {
+      host        = self.ip_address
+      type        = "ssh"
+      user        = "vagrant"
+      password    = "vagrant"
+    }
+  }
+  # <balise> 
+  network_interfaces {
+    virtual_network = var.vm_network
+    mac_address     = "<MACAddressLanPortGroup>"
+    nic_type        = "e1000"
+  }
+  # <balise>
+  network_interfaces {
+    virtual_network = var.hostonly_network
+    mac_address     = "<MACAddressHostOnly>"
+    nic_type        = "e1000"
+  }
+
+  guest_startup_timeout  = 45
+  guest_shutdown_timeout = 30
+}
